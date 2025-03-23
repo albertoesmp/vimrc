db1b78a72c71d35394396b19d2dab8cc9143483d
albertoesmp <albertoesmp@gmail.com>
2025-03-23 16:05:07 +0100

    Colors for topo-bathimetric LiDAR classifications.

--- context.sh
+++ context.sh
@@ -1,1 +1,2 @@
 alias gitlog="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+alias gpumon="nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv"
