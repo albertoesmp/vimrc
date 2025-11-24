cc117b101204347da467a604f5751842b3a04446
albertoesmp <albertoesmp@gmail.com>
2025-11-24 14:58:50 +0100

    Update with cloud compare colors and bashrc aliases in context.sh.

--- context.sh
+++ context.sh
@@ -1,2 +1,4 @@
 alias gitlog="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 alias gpumon="nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv"
+export VISUAL=vim
+export EDITOR=vim
